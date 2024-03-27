427b94bc6d984b81f8bcf0a4247597b2ce0faf15
André Santiago <82945862+Dresovense@users.noreply.github.com>
2024-03-27 11:28:31 +0100

    feat create scraping class

--- database/init_db.py
+++ database/init_db.py
@@ -8,13 +8,6 @@ with open('database\schema.sql') as f:
 
 cur = connection.cursor()
 
-cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
-            ('First Post', 'Content for the first post')
-            )
-
-cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
-            ('Second Post', 'Content for the second post')
-            )
 
 connection.commit()
 connection.close()
