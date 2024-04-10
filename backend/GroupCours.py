ccc7dd0a23d0a2d7ad42bb08c46b7020bd4645bc
André Santiago <82945862+Dresovense@users.noreply.github.com>
2024-04-10 14:35:27 +0200

    feat base backend

---
+++ backend/GroupCours.py
@@ -0,0 +1,13 @@
+class GroupCours():
+    def __init__(self, data:dict):
+        #create dataframe
+        pass
+
+    def similarity(self, group_cours):
+        pass
+
+    def send_data(self):
+        pass
+
+    def export_data(self, type="csv"):
+        pass
