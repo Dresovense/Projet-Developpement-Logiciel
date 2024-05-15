45729360767c4cbaaebfebca9846aa2c1a146087
Aarzawa <marie.fasel@unil.ch>
2024-05-15 14:51:47 +0200

    feat backend_comm

--- frontend/backend_comm.js
+++ frontend/backend_comm.js
@@ -23,25 +23,31 @@ function startDataCallback() {
         console.log("Data received!");
         jsonData = JSON.parse(xhr.responseText);
         
-        //création des options pour le choix des branches
-        cours_list = jsonData.cours;
-        selectList = document.body.getElementsByClassName("filtre_branche");
-        for(let j = 0; j< selectList.length; j++){
+        createBrancheChoice(jsonData)
+        createIntervenantChoice(jsonData)
+    }
+}
 
-            for (let i = 0; i< cours_list.length; i++){
-                var option = document.createElement("option");
-                option.text = cours_list[i][0];
-                option.id = cours_list[i][1];
-                selectList[j].add(option);
-            }
+//création des options pour le choix des branches
+function createBrancheChoice(jsonData) {
+    cours_list = jsonData.cours;
+    selectList = document.body.getElementsByClassName("filtre_branche");
+    for(let j = 0; j< selectList.length; j++){
+        for (let i = 0; i< cours_list.length; i++){
+            var option = document.createElement("option");
+            option.text = cours_list[i][0];
+            option.id = cours_list[i][1];
+            selectList[j].add(option);
         }
+    }
+}
 
-        //création des options pour le choix des intervenants
-        intervenant_list = jsonData.intervenants;
+//création des options pour le choix des intervenants
+function createIntervenantChoice(jsonData) {
+    intervenant_list = jsonData.intervenants;
         intervFiltre = document.body.getElementsByClassName("filtre_intervenant");
         console.log(intervFiltre);
         for(let j = 0; j< intervFiltre.length; j++){
-
             for (let i = 0; i< intervenant_list.length; i++){
                 var option = document.createElement("option");
                 option.text = intervenant_list[i][0];
@@ -49,7 +55,6 @@ function startDataCallback() {
                 intervFiltre[j].add(option);
             }
         }
-    }
 }
 
 let test_data = {
