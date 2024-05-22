91fe057a2e04d747db397fe8738beac11cd2484d
Aarzawa <marie.fasel@unil.ch>
2024-05-22 13:54:25 +0200

    doc + delete useless filters in search

--- backend/GroupCours.py
+++ backend/GroupCours.py
@@ -1,3 +1,17 @@
+"""
+This module defines the GroupCours class, which handles course data and calculates similarities between courses using a Doc2Vec model. It also provides functionality to export data and build a new Doc2Vec model.
+
+Classes:
+    GroupCours: Manages course data, calculates similarities, exports data, and builds Doc2Vec models.
+
+Methods:
+    __init__(self, data: list): Initializes the GroupCours object with course data.
+    similarity(self, group_cours_df, cluster_type: str = "mean"): Calculates similarity between courses.
+    send_data(self): Placeholder method for future data sending functionality.
+    export_data(self, filename: str, type: str = "xlsx"): Exports course data to a file.
+    build_model(self): Builds a new Doc2Vec model using course data.
+"""
+
 import pandas as pd
 import re
 import gensim
@@ -6,39 +20,44 @@ from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 from nltk.tokenize import word_tokenize
 from statistics import mean, median
 
-class GroupCours():
-    def __init__(self, data:list):
-        #create dataframe
+class GroupCours:
+    def __init__(self, data: list):
+        """
+        Initializes the GroupCours object with course data.
+
+        Args:
+            data (list): List of course data dictionaries.
+        """
         self.dataframe = pd.DataFrame(data)
         print(self.dataframe)
 
-    def similarity(self, group_cours_df, cluster_type:str = "mean"):
+    def similarity(self, group_cours_df, cluster_type: str = "mean"):
+        """
+        Calculates similarity between courses using a Doc2Vec model and cosine similarity.
 
-        # Example documents
+        Args:
+            group_cours_df (GroupCours): Another GroupCours object containing courses to compare against.
+            cluster_type (str, optional): Method to aggregate similarities ('mean', 'max', 'min', 'median'). Defaults to "mean".
 
-        corpus_wanted = self.dataframe['nom'].to_list() #list of the classes we want to test the similarity for
-        corpus_query = group_cours_df.dataframe['nom'].to_list()  #list of the classes we want to base the similiarity on
+        Raises:
+            Exception: If an invalid cluster_type is provided.
+        """
+        # Extract course names
+        corpus_wanted = self.dataframe['nom'].to_list()
+        corpus_query = group_cours_df.dataframe['nom'].to_list()
 
-
-        #load model
+        # Load the pre-trained Doc2Vec model
         model = Doc2Vec.load("backend/d2v.model")
 
-        # Infer vectors for user-selected documents
+        # Infer vectors for user-selected and query documents
         wanted_vecs = [model.infer_vector(word_tokenize(doc.lower())) for doc in corpus_wanted]
-
-        # Infer vectors for all other documents
         query_vecs = [model.infer_vector(word_tokenize(doc.lower())) for doc in corpus_query]
 
-        # Compute cosine similarity
+        # Compute cosine similarity matrix
         similarity_matrix = cosine_similarity(wanted_vecs, query_vecs)
 
-        # Output the similarities (each row corresponds to a user doc, each column to an other doc)
-        print(f"Wanted vecs: {len(wanted_vecs)}")
-        print(f"Query vecs : {len(query_vecs)}")
-        print(f"Similairty matrix: {len(similarity_matrix)}")
-
-        #Input similarities in dataframe
-        similarity_list = list()
+        # Aggregate similarities based on the specified cluster type
+        similarity_list = []
         for vec in similarity_matrix:
             if cluster_type == "mean":
                 similarity_list.append(mean(vec))
@@ -50,52 +69,49 @@ class GroupCours():
                 similarity_list.append(median(vec))
             else:
                 raise Exception(f"{cluster_type} is not an acceptable string")
+        
         self.dataframe = self.dataframe.assign(similarity=similarity_list)
-
         self.dataframe = self.dataframe.sort_values(by=['similarity'])
 
         print(self.dataframe[['nom', 'similarity']].head(10))
         print(self.dataframe.nlargest(10, 'similarity'))
-        #print(self.dataframe[['nom', 'similarity']].head(10))
-        #print(self.dataframe.nlargest(10, 'similarity'))
+
         self.export_data("my_results", "xlsx")
 
-    # A ENLEVER ?
-    def send_data(self):
-        pass
+    def export_data(self, filename: str, type: str = "xlsx"):
+        """
+        Exports course data to a file.
 
-    def export_data(self, filename:str, type:str="xlsx"):
-
+        Args:
+            filename (str): Name of the file to export.
+            type (str, optional): Type of the file to export ('xlsx' or 'csv'). Defaults to "xlsx".
+        """
         if type == "xlsx":
             self.dataframe.to_excel(f"{filename}.xlsx", columns=["nom", "langage", "credits", "url", "similarity", "prog_op"])
-        if type == "csv":
+        elif type == "csv":
             self.dataframe.to_csv(f"{filename}.csv", columns=["nom", "langage", "credits", "url", "similarity", "prog_op"])
 
-
     def build_model(self):
-        #create corpus
-        corpus = dict()
+        """
+        Builds a new Doc2Vec model using course data.
+        """
+        # Create corpus from course data
+        corpus = {}
         for _, course in self.dataframe.iterrows():
-            course_str = list()
-            if course['objectif'] != "":
+            course_str = []
+            if course['objectif']:
                 course_str.append(course['objectif'])
-            if course['contenu'] != "":
+            if course['contenu']:
                 course_str.append(course['contenu'])
             course_str = "\n".join(course_str)
-            #clean string
             course_str = re.sub(r'\s+', ' ', re.sub(r'\xa0|\*\*\*', '', course_str.strip()))
-            #add to corpus
             corpus[course['nom']] = course_str
 
-        #print(corpus)
-        
-        #tag data
-        tagged_data = list()
-        for key, value in corpus.items():
-            tagged_data.append(TaggedDocument(words=word_tokenize(value.lower()), tags=[key]))
+        # Tag data for Doc2Vec
+        tagged_data = [TaggedDocument(words=word_tokenize(value.lower()), tags=[key]) for key, value in corpus.items()]
         print(tagged_data)
 
-        #build model
+        # Build and train the Doc2Vec model
         model = gensim.models.doc2vec.Doc2Vec(vector_size=50, min_count=1, epochs=40)
         model.build_vocab(tagged_data)
         model.train(tagged_data, total_examples=model.corpus_count, epochs=80)
--- backend/Search.py
+++ backend/Search.py
@@ -1,111 +1,95 @@
+"""
+This module defines the Search class, which handles retrieving and filtering course data from an SQLite database based on specified criteria such as intervenants and branches.
+
+Classes:
+    Search: Provides methods for retrieving and filtering course data.
+
+Methods:
+    get_data(self, intervenants: list = None, branches: list = None, return_others: bool = False): Retrieves and filters course data based on specified criteria.
+    __check_intervenants(self, course, intervenants): Checks if the course intervenants match any of the specified intervenants.
+    __check_branches(self, course, branches): Checks if the course branch matches any of the specified branches.
+    __get_all_courses(self): Retrieves all courses from the database along with their associated horaires, intervenants, and branches.
+"""
+
 import sqlite3
 
-class Search():
+class Search:
     db_info = 'database/database.db'
 
-    def get_data(self, languages:list = None, credits:list = None, intervenants:list = None, branches:list = None, semester: list = None, horaires:list = None, return_others:bool = False):
+    def get_data(self, intervenants: list = None, branches: list = None, return_others: bool = False):
+        """
+        Retrieves and filters course data based on specified criteria.
+
+        Args:
+            intervenants (list, optional): List of intervenants to filter by.
+            branches (list, optional): List of branches to filter by.
+            return_others (bool, optional): Whether to return courses not matching the filters. Defaults to False.
+
+        Returns:
+            list: Filtered courses.
+            list (optional): Courses not matching the filters if return_others is True.
+        """
         other_courses = self.__get_all_courses()
-        courses_filtered = list()
+        courses_filtered = []
 
-        #iterate through the courses
+        # Iterate through the courses
         for course in other_courses[:]:
-            #apply language filter if specified
-            if languages:
-                if self.__check_languages(course, languages):
-                    if course in other_courses:
-                        other_courses.remove(course)
-                        courses_filtered.append(course)
+            if intervenants and self.__check_intervenants(course, intervenants):
+                if course in other_courses:
+                    other_courses.remove(course)
+                    courses_filtered.append(course)
+            if branches and self.__check_branches(course, branches):
+                if course in other_courses:
+                    other_courses.remove(course)
+                    courses_filtered.append(course)
 
-            #apply credits filter if specified
-            if credits:
-                if self.__check_credits(course, credits):
-                    if course in other_courses:
-                        other_courses.remove(course)
-                        courses_filtered.append(course)
-
-            #apply intervenants filter if specified
-            if intervenants:
-                if self.__check_intervenants(course, intervenants):
-                    if course in other_courses:
-                        other_courses.remove(course)
-                        courses_filtered.append(course)
-
-            #apply branches filter if specified
-            if branches:
-                if self.__check_branches(course, branches):
-                    if course in other_courses:
-                        other_courses.remove(course)
-                        courses_filtered.append(course)
-
-            #apply semester filter if specified
-            if semester:
-                if self.__check_semesters(course, semester):
-                    if course in other_courses:
-                        other_courses.remove(course)
-                        courses_filtered.append(course)
-
-            #apply horaire filter if specified
-            if horaires:
-                if self.__check_horaires(course, horaires):
-                    if course in other_courses:
-                        other_courses.remove(course)
-                        courses_filtered.append(course)
-        
         if return_others:
             return courses_filtered, other_courses
-        
+
         return courses_filtered
-            
 
-    def __check_languages(self, course, languages):
-        if course["langage"] in languages:
-            return True
-        return False
-    
-    def __check_credits(self, course, credits):
-        if course["credits"] in credits:
-            return True
-        return False
-    
     def __check_intervenants(self, course, intervenants):
-        # in the case of a list of intervenants in arg, returns true if one of them is in the course["intervenants"]
-        if set(course["intervenants"]).intersection(set(intervenants)):
-            return True
-        return False
-    
+        """
+        Checks if the course intervenants match any of the specified intervenants.
+
+        Args:
+            course (dict): Course data.
+            intervenants (list): List of intervenants to check against.
+
+        Returns:
+            bool: True if there is a match, False otherwise.
+        """
+        return bool(set(course["intervenants"]).intersection(set(intervenants)))
+
     def __check_branches(self, course, branches):
-        if course["branche"] in branches:
-            return True
-        return False
-    
-    def __check_semesters(self, course, semesters):
-        if course["semestre"] in semesters:
-            return True
-        return False
-    
-    def __check_horaires(self, course, horaires):
-        pass
-        """ if set(course["horaire"]) in horaires: """
+        """
+        Checks if the course branch matches any of the specified branches.
 
-    def __get_all_courses(self):  
-        #database connection
+        Args:
+            course (dict): Course data.
+            branches (list): List of branches to check against.
+
+        Returns:
+            bool: True if there is a match, False otherwise.
+        """
+        return course["branche"] in branches
+
+    def __get_all_courses(self):
+        """
+        Retrieves all courses from the database along with their associated horaires, intervenants, and branches.
+
+        Returns:
+            list: List of courses with associated data.
+        """
         conn = sqlite3.connect(self.db_info)
-
-        # Set the row factory to sqlite3.Row
         conn.row_factory = sqlite3.Row
 
-        # Fetch all rows from the 'cours' table
         cours_rows = conn.execute('SELECT * FROM cours').fetchall()
-
-        # Initialize a list to store the dictionaries
         cours_dict_list = []
 
-        # Iterate over each course row
         for row in cours_rows:
-            # Create a dictionary for the current course
             cours_dict = dict(row)
-            
-            # Fetch horaireids for the current course
+
             horaireids_query = '''
                 SELECT horaireid
                 FROM cours_has_horaire
@@ -113,9 +97,7 @@ class Search():
             '''
             horaireids = conn.execute(horaireids_query, (cours_dict['id'],)).fetchall()
             cours_dict['horaires'] = [horaireid['horaireid'] for horaireid in horaireids]
-    
-            
-            # Fetch intervenants for the current course
+
             intervenants_query = '''
                 SELECT intervenant.nom
                 FROM intervenant
@@ -125,7 +107,6 @@ class Search():
             intervenants = conn.execute(intervenants_query, (cours_dict['id'],)).fetchall()
             cours_dict['intervenants'] = [intervenant['nom'] for intervenant in intervenants]
 
-            # Fetch the branch name for the current course
             branche_query = '''
                 SELECT nom
                 FROM branche
@@ -134,10 +115,9 @@ class Search():
             branche = conn.execute(branche_query, (cours_dict['brancheid'],)).fetchone()
             if branche:
                 cours_dict['branche'] = branche['nom']
-            
-            # Append the course dictionary to the list
+
             cours_dict_list.append(cours_dict)
-        
+
         conn.close()
 
         return cours_dict_list
